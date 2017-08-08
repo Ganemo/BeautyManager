0fc6b8d0d901354efc8bf71354821c35b4f770fc
Ganemo <ganemogiannis@knights.ucf.edu>
2017-08-08 14:12:23 -0400

    Speechlet Removal

--- BeautyManager/src/main/java/com/amazonaws/lambda/ganemo/LambdaFunctionHandler.java
+++ BeautyManager/src/main/java/com/amazonaws/lambda/ganemo/LambdaFunctionHandler.java
@@ -3,7 +3,7 @@ package com.amazonaws.lambda.ganemo;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 
-public class LambdaFunctionHandler implements RequestHandler<Object, String> {
+public class LambdaFunctionHandler implements RequestHandler<Object, String>, Speechlet {
 
     @Override
     public String handleRequest(Object input, Context context) {
