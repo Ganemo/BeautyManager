28faac7ee854cde7b08ed089e1e24244c687bda1
Ganemo <ganemogiannis@knights.ucf.edu>
2017-08-09 18:28:09 -0400

    DynamoDB Access working

--- BeautyManager/src/main/java/com/amazonaws/lambda/ganemo/DynamoDBTesting.java
+++ BeautyManager/src/main/java/com/amazonaws/lambda/ganemo/DynamoDBTesting.java
@@ -1,43 +1,75 @@
 package com.amazonaws.lambda.ganemo;
 
-import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
-import com.amazonaws.SDKGlobalConfiguration;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
 import com.amazonaws.services.dynamodbv2.document.DynamoDB;
+import com.amazonaws.services.dynamodbv2.document.Item;
+import com.amazonaws.services.dynamodbv2.document.PutItemOutcome;
 import com.amazonaws.services.dynamodbv2.document.Table;
-import com.amazonaws.services.dynamodbv2.model.AttributeDefinition;
-import com.amazonaws.services.dynamodbv2.model.CreateTableRequest;
-import com.amazonaws.services.dynamodbv2.model.KeySchemaElement;
-import com.amazonaws.services.dynamodbv2.model.KeyType;
-import com.amazonaws.services.dynamodbv2.model.ProvisionedThroughput;
+import com.amazonaws.services.dynamodbv2.document.spec.GetItemSpec;
 
 public class DynamoDBTesting {
 
 	public static void main(String[] args) {
-		SDKGlobalConfiguration.isInRegionOptimizedModeEnabled();
 		AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard().withRegion(Regions.US_EAST_1).build();
-		
-		 DynamoDB dynamoDB = new DynamoDB(client);
-		 
-		 String tableName = "BeautyRegiment";
-		 
-		 try {
-			 CreateTableRequest request = new CreateTableRequest()
-					 .withTableName(tableName)
-					 .withKeySchema(Arrays.asList(new KeySchemaElement().withAttributeName("Id").withKeyType(KeyType.HASH)))
-					 .withAttributeDefinitions(Arrays.asList(new AttributeDefinition().withAttributeName("Id").withAttributeType("N")))
-					 .withProvisionedThroughput(new ProvisionedThroughput().withReadCapacityUnits(5L).withWriteCapacityUnits(6L));
-			 
-			 Table table = dynamoDB.createTable(request);
-			 table.waitForActive();
-			 System.out.println(table.getTableName());
-		 } catch (Exception e) {
-			 System.err.println("Unable to create table: ");
-	         System.err.println(e.getMessage());
-		 }
+
+		DynamoDB dynamoDB = new DynamoDB(client);
+
+		String tableName = "Movies";
+
+		try {
+			/*
+			 * CreateTableRequest request = new CreateTableRequest()
+			 * 		.withTableName(tableName)
+					.withKeySchema(Arrays.asList(new KeySchemaElement()
+						.withAttributeName("Id")
+						.withKeyType(KeyType.HASH)))
+					.withAttributeDefinitions(Arrays.asList(new AttributeDefinition()
+						.withAttributeName("Id")
+						.withAttributeType("N")))
+					.withProvisionedThroughput(new ProvisionedThroughput()
+						.withReadCapacityUnits(5L)
+						.withWriteCapacityUnits(6L));
+			 * 
+			 * Table table = dynamoDB.createTable(request);
+			 */
+			/*
+			 * Table table = dynamoDB.createTable(tableName, Arrays.asList(
+			 * 		new KeySchemaElement("year", KeyType.HASH), // Partition // key
+			 * 		new KeySchemaElement("title", KeyType.RANGE)), // Sort key
+			 * Arrays.asList(
+			 * 		new AttributeDefinition("year", ScalarAttributeType.N),
+			 * 		new AttributeDefinition("title", ScalarAttributeType.S)),
+			 * 		new ProvisionedThroughput(10L, 10L));
+			 */
+
+			Table table = dynamoDB.getTable("Movies");
+
+			table.waitForActive();
+			System.out.println(table.getTableName() + "Created and ACTIVE");
+
+			/*Map<String, Object> infoMap = new HashMap<String, Object>();
+			infoMap.put("plot", "Nothing happens at all.");
+			infoMap.put("rating", 0);
+
+			System.out.println("Adding a new item...");
+			PutItemOutcome outcome = table.putItem(
+					new Item().withPrimaryKey("year", 1999, "title", "TheMovieOfMovies").withMap("info", infoMap));
+
+			System.out.println("PutItem succeeded:\n" + outcome.getPutItemResult());
+			*/
+			System.out.println("Attempting to read the item...");
+            Item getoutcome = table.getItem(new GetItemSpec().withPrimaryKey("year", 1999, "title", "TheMovieOfMovies"));
+            
+            System.out.println("GetItem succeeded: " + (String)getoutcome.asMap().get("title"));
+		} catch (Exception e) {
+			System.err.println("Unable to create table: ");
+			System.err.println(e.getMessage());
+		}
 	}
 
 }
--- BeautyManager/src/main/java/com/amazonaws/lambda/ganemo/HelloWorldSpeechlet.java
+++ BeautyManager/src/main/java/com/amazonaws/lambda/ganemo/HelloWorldSpeechlet.java
@@ -9,10 +9,10 @@
  */
 package com.amazonaws.lambda.ganemo;
 
+import java.util.Map;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-
 
 import com.amazon.speech.slu.Intent;
 import com.amazon.speech.speechlet.IntentRequest;
@@ -26,125 +26,137 @@ import com.amazon.speech.speechlet.SpeechletResponse;
 import com.amazon.speech.ui.PlainTextOutputSpeech;
 import com.amazon.speech.ui.Reprompt;
 import com.amazon.speech.ui.SimpleCard;
-
 import com.amazonaws.regions.Regions;
-import com.amazonaws.services.dynamodbv2.*;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.document.DynamoDB;
+import com.amazonaws.services.dynamodbv2.document.Item;
+import com.amazonaws.services.dynamodbv2.document.Table;
+import com.amazonaws.services.dynamodbv2.document.spec.GetItemSpec;
 
 /**
- * This sample shows how to create a simple speechlet for handling speechlet requests.
+ * This sample shows how to create a simple speechlet for handling speechlet
+ * requests.
  */
 public class HelloWorldSpeechlet implements Speechlet {
-    private static final Logger log = LoggerFactory.getLogger(HelloWorldSpeechlet.class);
+	private static final Logger log = LoggerFactory.getLogger(HelloWorldSpeechlet.class);
 
-    @Override
-    public void onSessionStarted(final SessionStartedRequest request, final Session session)
-            throws SpeechletException {
-        log.info("onSessionStarted requestId={}, sessionId={}", request.getRequestId(),
-                session.getSessionId());
-        // any initialization logic goes here
-        
-       
-    }
+	private DynamoDB dynamoDB;
+	private String requestId;
+	
+	@Override
+	public void onSessionStarted(final SessionStartedRequest request, final Session session) throws SpeechletException {
+		log.info("onSessionStarted requestId={}, sessionId={}", request.getRequestId(), session.getSessionId());
+		// any initialization logic goes here
+		dynamoDB = new DynamoDB(AmazonDynamoDBClientBuilder.standard().withRegion(Regions.US_EAST_1).build());
+		requestId = request.getRequestId();
+	}
 
-    @Override
-    public SpeechletResponse onLaunch(final LaunchRequest request, final Session session)
-            throws SpeechletException {
-        log.info("onLaunch requestId={}, sessionId={}", request.getRequestId(),
-                session.getSessionId());
-        return getWelcomeResponse();
-    }
+	@Override
+	public SpeechletResponse onLaunch(final LaunchRequest request, final Session session) throws SpeechletException {
+		log.info("onLaunch requestId={}, sessionId={}", request.getRequestId(), session.getSessionId());
+		return getWelcomeResponse();
+	}
 
-    @Override
-    public SpeechletResponse onIntent(final IntentRequest request, final Session session)
-            throws SpeechletException {
-        log.info("onIntent requestId={}, sessionId={}", request.getRequestId(),
-                session.getSessionId());
+	@Override
+	public SpeechletResponse onIntent(final IntentRequest request, final Session session) throws SpeechletException {
+		log.info("onIntent requestId={}, sessionId={}", request.getRequestId(), session.getSessionId());
 
-        Intent intent = request.getIntent();
-        String intentName = (intent != null) ? intent.getName() : null;
-        
-        if ("BeautyRegimen".equals(intentName)) {
-            return getHelloResponse();
-        } else if ("AMAZON.HelpIntent".equals(intentName)) {
-            return getHelpResponse();
-        } else {
-            throw new SpeechletException("Invalid Intent");
-        }
-    }
+		Intent intent = request.getIntent();
+		String intentName = (intent != null) ? intent.getName() : null;
 
-    @Override
-    public void onSessionEnded(final SessionEndedRequest request, final Session session)
-            throws SpeechletException {
-        log.info("onSessionEnded requestId={}, sessionId={}", request.getRequestId(),
-                session.getSessionId());
-        // any cleanup logic goes here
-    }
+		if ("BeautyRegimen".equals(intentName)) {
+			return getHelloResponse();
+		} else if ("AMAZON.HelpIntent".equals(intentName)) {
+			return getHelpResponse();
+		} else {
+			throw new SpeechletException("Invalid Intent");
+		}
+	}
 
-    /**
-     * Creates and returns a {@code SpeechletResponse} with a welcome message.
-     *
-     * @return SpeechletResponse spoken and visual response for the given intent
-     */
-    private SpeechletResponse getWelcomeResponse() {
-        String speechText = "Welcome to the Alexa Skills Kit, you can say hello";
+	@Override
+	public void onSessionEnded(final SessionEndedRequest request, final Session session) throws SpeechletException {
+		log.info("onSessionEnded requestId={}, sessionId={}", request.getRequestId(), session.getSessionId());
+		// any cleanup logic goes here
+	}
 
-        // Create the Simple card content.
-        SimpleCard card = new SimpleCard();
-        card.setTitle("HelloWorld");
-        card.setContent(speechText);
+	/**
+	 * Creates and returns a {@code SpeechletResponse} with a welcome message.
+	 *
+	 * @return SpeechletResponse spoken and visual response for the given intent
+	 */
+	private SpeechletResponse getWelcomeResponse() {
+		String speechText = "Welcome to the Alexa Skills Kit, you can say hello";
 
-        // Create the plain text output.
-        PlainTextOutputSpeech speech = new PlainTextOutputSpeech();
-        speech.setText(speechText);
+		// Create the Simple card content.
+		SimpleCard card = new SimpleCard();
+		card.setTitle("HelloWorld");
+		card.setContent(speechText);
 
-        // Create reprompt
-        Reprompt reprompt = new Reprompt();
-        reprompt.setOutputSpeech(speech);
+		// Create the plain text output.
+		PlainTextOutputSpeech speech = new PlainTextOutputSpeech();
+		speech.setText(speechText);
 
-        return SpeechletResponse.newAskResponse(speech, reprompt, card);
-    }
+		// Create reprompt
+		Reprompt reprompt = new Reprompt();
+		reprompt.setOutputSpeech(speech);
 
-    /**
-     * Creates a {@code SpeechletResponse} for the hello intent.
-     *
-     * @return SpeechletResponse spoken and visual response for the given intent
-     */
-    private SpeechletResponse getHelloResponse() {
-        String speechText = "Wash Your Nasty Face";
+		return SpeechletResponse.newAskResponse(speech, reprompt, card);
+	}
 
-        // Create the Simple card content.
-        SimpleCard card = new SimpleCard();
-        card.setTitle("WashYourFace");
-        card.setContent(speechText);
+	/**
+	 * Creates a {@code SpeechletResponse} for the hello intent.
+	 *
+	 * @return SpeechletResponse spoken and visual response for the given intent
+	 */
+	private SpeechletResponse getHelloResponse() {
+		String tableName = "Movies";
 
-        // Create the plain text output.
-        PlainTextOutputSpeech speech = new PlainTextOutputSpeech();
-        speech.setText(speechText);
+		Table table = dynamoDB.getTable("BeautyManagement");
+		
+		
 
-        return SpeechletResponse.newTellResponse(speech, card);
-    }
+		/*Item getoutcome = table.getItem(new GetItemSpec().withPrimaryKey("year", 1999, "title", "TheMovieOfMovies"));
 
-    /**
-     * Creates a {@code SpeechletResponse} for the help intent.
-     *
-     * @return SpeechletResponse spoken and visual response for the given intent
-     */
-    private SpeechletResponse getHelpResponse() {
-        String speechText = "You can say hello to me!";
+		Map<String, Object> mp = getoutcome.asMap();
 
-        // Create the Simple card content.
-        SimpleCard card = new SimpleCard();
-        card.setTitle("HelloWorld");
-        card.setContent(speechText);
+		String speechText = (String) mp.get("title");*/
 
-        // Create the plain text output.
-        PlainTextOutputSpeech speech = new PlainTextOutputSpeech();
-        speech.setText(speechText);
+		//String speechText = "Hello There";
 
-        // Create reprompt
-        Reprompt reprompt = new Reprompt();
-        reprompt.setOutputSpeech(speech);
+		// Create the Simple card content.
+		SimpleCard card = new SimpleCard();
+		card.setTitle("WashYourFace");
+		card.setContent(requestId);
 
-        return SpeechletResponse.newAskResponse(speech, reprompt, card);
-    }
+		// Create the plain text output.
+		PlainTextOutputSpeech speech = new PlainTextOutputSpeech();
+		speech.setText(requestId);
+
+		return SpeechletResponse.newTellResponse(speech, card);
+	}
+
+	/**
+	 * Creates a {@code SpeechletResponse} for the help intent.
+	 *
+	 * @return SpeechletResponse spoken and visual response for the given intent
+	 */
+	private SpeechletResponse getHelpResponse() {
+		String speechText = "You can say hello to me!";
+
+		// Create the Simple card content.
+		SimpleCard card = new SimpleCard();
+		card.setTitle("HelloWorld");
+		card.setContent(speechText);
+
+		// Create the plain text output.
+		PlainTextOutputSpeech speech = new PlainTextOutputSpeech();
+		speech.setText(speechText);
+
+		// Create reprompt
+		Reprompt reprompt = new Reprompt();
+		reprompt.setOutputSpeech(speech);
+
+		return SpeechletResponse.newAskResponse(speech, reprompt, card);
+	}
 }
